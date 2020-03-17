cf015d276ba398c1e8edb69d21f5f0bf11365953
Sigit <noprisigit@gmail.com>
2020-03-17 14:31:11 +0700

    create method get_all_print_shop

--- application/models/OwnerModel.php
+++ application/models/OwnerModel.php
@@ -1,5 +1,10 @@
 <?php
 
 class OwnerModel extends CI_Model {
-    
+    public function get_all_print_shop() {
+        $this->db->select('*');
+        $this->db->from('partners');
+        $this->db->join('users', 'partners.id_user = users.id_user');
+        return $this->db->get()->result_array();
+    }
 }
