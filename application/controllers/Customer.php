c186b41b861c60c254af477d167a6affe4177479
Sigit <noprisigit@gmail.com>
2020-03-17 23:06:26 +0700

    configure method index

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -2,5 +2,21 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Customer extends CI_Controller {
-    
+    public function __construct() {
+        parent::__construct();
+        if(!$this->session->userdata('email')) {
+            redirect('auth');
+        }
+    }
+
+    public function index() {
+        $header['title'] = "Home";
+        $header['access'] = "Customer";
+
+        // $content['status_toko'] = $this->db->get_where('partners', ['id_user' => $this->session->userdata('id_user')])->row_array();
+        // dd($content['status_toko']);
+        $this->load->view('template/header', $header);
+        $this->load->view('customer/dashboard');
+        $this->load->view('template/footer');
+    }
 }
