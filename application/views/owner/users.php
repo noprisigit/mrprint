48b070e32b919cb0b983275b4b70251cc263abf5
Sigit <noprisigit@gmail.com>
2020-03-17 12:46:46 +0700

    update users view

--- application/views/owner/users.php
+++ application/views/owner/users.php
@@ -4,7 +4,7 @@
     <div class="card">
         <!-- /.card-header -->
         <div class="card-body">
-            <table class="table table-bordered table-striped data-table">
+            <table class="table table-bordered data-table">
                 <thead>
                     <tr>
                         <th class="text-center">ID</th>
@@ -17,11 +17,23 @@
                 <tbody>
                     <?php foreach($users as $item) : ?>
                     <tr>
-                        <td><?= $item['id_user']; ?></td>
+                        <td class="text-center"><?= $item['id_user']; ?></td>
                         <td><?= $item['full_name']; ?></td>
                         <td><?= $item['email']; ?></td>
-                        <td><?= $item['status_account']; ?></td>
-                        <td></td>
+                        <?php if($item['status_account'] == 0) : ?>
+                            <td class="text-center text-danger">Not Active</td>
+                        <?php else : ?>
+                            <td class="text-center text-success">Active</td>
+                        <?php endif; ?>
+                        <td class="text-center">
+                            <div class="btn-group">
+                                <span data-toggle="modal" data-target="#modal-detail">
+                                    <button type="button" class="btn btn-info" data-toggle="tooltip" data-placement="top" title="Detail" data-nama="<?= $item['full_name']; ?>" data-username="<?= $item['username']; ?>" data-email="<?= $item['email']; ?>" data-akses="<?= $item['status_access']; ?>" data-akun="<?= $item['status_account']; ?>"><i class="fas fa-info-circle"></i></button>
+                                </span>
+                                <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Block"><i class="fas fa-ban"></i></button>
+                                <a href="<?= base_url('owner/delete-user/') . $item['id_user']; ?>" onclick="return confirm('Anda yakin ingin menghapus ini ?')" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash"></i></a>
+                            </div>
+                        </td>
                     </tr>
                     <?php endforeach; ?>
                 </tbody>
@@ -33,3 +45,47 @@
 
 </section>
 <!-- /.content -->
+
+<div class="modal fade" id="modal-detail" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Detail User</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="card-body">
+                  
+                    <div class="form-group">
+                        <label for="">Nama Lengkap</label>
+                        <input type="text" class="form-control detail-nama" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="text" class="form-control detail-email" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Username</label>
+                        <input type="text" class="form-control detail-username"readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Status Akses</label>
+                        <input type="text" class="form-control detail-akses" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Status Akun</label>
+                        <input type="text" class="form-control detail-akun" readonly>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
