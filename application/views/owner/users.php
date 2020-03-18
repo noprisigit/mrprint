e398fd7e34c5bf7a35f2b373ad39f4fdc9bd7acc
Sigit <noprisigit@gmail.com>
2020-03-19 00:17:01 +0700

    add users file

--- application/views/owner/users.php
+++ application/views/owner/users.php
@@ -19,29 +19,31 @@
                             </thead>
                             <tbody>
                                 <?php foreach($users as $item) : ?>
-                                <tr>
-                                    <td class="text-center"><?= $item['id_user']; ?></td>
-                                    <td><?= $item['full_name']; ?></td>
-                                    <td><?= $item['email']; ?></td>
-                                    <?php if($item['status_account'] == 0) : ?>
-                                        <td class="text-center text-danger">Not Active</td>
-                                    <?php else : ?>
-                                        <td class="text-center text-success">Active</td>
-                                    <?php endif; ?>
-                                    <td class="text-center">
-                                        <div class="btn-group">
-                                            <span data-toggle="modal" data-target="#modal-detail">
-                                                <button type="button" class="btn btn-info btn-detail-user" data-toggle="tooltip" data-placement="top" title="Detail" data-nama="<?= $item['full_name']; ?>" data-username="<?= $item['username']; ?>" data-email="<?= $item['email']; ?>" data-akses="<?= $item['status_access']; ?>" data-akun="<?= $item['status_account']; ?>"><i class="fas fa-info-circle"></i></button>
-                                            </span>
-                                            <?php if($item['status_account'] == 0) : ?>
-                                                <a href="<?= base_url('owner/unblock-user/'). $item['id_user']; ?>" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Unblock"><i class="fas fa-check-circle"></i></a>
-                                            <?php else: ?>
-                                                <a href="<?= base_url('owner/block-user/'). $item['id_user']; ?>" onclick="return confirm('Anda yakin ingin memblokir akun ini ?')" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Block"><i class="fas fa-ban"></i></a>
-                                            <?php endif; ?>
-                                            <a href="<?= base_url('owner/delete-user/') . $item['id_user']; ?>" onclick="return confirm('Anda yakin ingin menghapus ini ?')" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash"></i></a>
-                                        </div>
-                                    </td>
-                                </tr>
+                                    <?php if($item['status_access'] != "owner") : ?>
+                                    <tr>
+                                        <td class="text-center"><?= $item['id_user']; ?></td>
+                                        <td><?= $item['full_name']; ?></td>
+                                        <td><?= $item['email']; ?></td>
+                                        <?php if($item['status_account'] == 0) : ?>
+                                            <td class="text-center text-danger">Not Active</td>
+                                        <?php else : ?>
+                                            <td class="text-center text-success">Active</td>
+                                        <?php endif; ?>
+                                        <td class="text-center">
+                                            <div class="btn-group">
+                                                <span data-toggle="modal" data-target="#modal-detail">
+                                                    <button type="button" class="btn btn-info btn-detail-user" data-toggle="tooltip" data-placement="top" title="Detail" data-nama="<?= $item['full_name']; ?>" data-username="<?= $item['username']; ?>" data-email="<?= $item['email']; ?>" data-akses="<?= $item['status_access']; ?>" data-akun="<?= $item['status_account']; ?>"><i class="fas fa-info-circle"></i></button>
+                                                </span>
+                                                <?php if($item['status_account'] == 0) : ?>
+                                                    <a href="<?= base_url('owner/unblock-user/'). $item['id_user']; ?>" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Unblock"><i class="fas fa-check-circle"></i></a>
+                                                <?php else: ?>
+                                                    <a href="<?= base_url('owner/block-user/'). $item['id_user']; ?>" onclick="return confirm('Anda yakin ingin memblokir akun ini ?')" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Block"><i class="fas fa-ban"></i></a>
+                                                <?php endif; ?>
+                                                <a href="<?= base_url('owner/delete-user/') . $item['id_user']; ?>" onclick="return confirm('Anda yakin ingin menghapus ini ?')" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php endif;?>
                                 <?php endforeach; ?>
                             </tbody>
                         </table>
