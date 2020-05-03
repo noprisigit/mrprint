71303a5e1ad6e2596809c6da2d3b505afa699575
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-03 17:32:45 +0700

    add file api customer and customer_model

---
+++ application/controllers/api/Customer.php
@@ -0,0 +1,11 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Customer extends REST_Controller
+{
+    
+}
