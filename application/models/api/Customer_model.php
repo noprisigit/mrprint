d40eb19e3b4dd8d179630245767a9e7042bd9a49
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-03 20:11:16 +0700

    configure model method for get list print shop

--- application/models/api/Customer_model.php
+++ application/models/api/Customer_model.php
@@ -14,4 +14,15 @@ class Customer_model extends CI_Model {
             return $this->db->get_where('list_kabupaten', ['id_provinsi' => $id_provinsi])->result_array();
         }
     }
+
+    public function getAllPrintShop($id_provinsi, $id_kabupaten)
+    {
+        $this->db->select('*');
+        $this->db->from('partners');
+        $this->db->join('list_provinsi', 'partners.provinsi = list_provinsi.id_provinsi');
+        $this->db->join('list_kabupaten', 'partners.kabupaten = list_kabupaten.id_kabupaten');
+        $this->db->where('provinsi', $id_provinsi);
+        $this->db->where('kabupaten', $id_kabupaten);
+        return $this->db->get()->result_array();
+    }
 }
