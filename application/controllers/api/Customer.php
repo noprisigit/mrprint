a32e1b7965bbc9d2ea1c37389ef88868e45bbc01
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-03 19:58:03 +0700

    configure api for get provinsi and kabupaten

--- application/controllers/api/Customer.php
+++ application/controllers/api/Customer.php
@@ -12,4 +12,36 @@ class Customer extends REST_Controller
         parent::__construct();
         $this->load->model('api/Customer_model', 'customer');
     }
+
+    public function list_provinsi_get()
+    {
+        $list_provinsi = $this->customer->getListProvinsi();
+
+        if ( $list_provinsi ) {
+            $this->response([
+                'status'    => TRUE,
+                'message'   => 'List province successfully loaded',
+                'data'      => $list_provinsi
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function list_kabupaten_get()
+    {
+        $id_provinsi = $this->get('id_provinsi');
+
+        if ( $id_provinsi === NULL ) {
+            $list_kabupaten = $this->customer->getListKabupaten();
+        } else {
+            $list_kabupaten = $this->customer->getListKabupaten($id_provinsi);
+        }
+
+        if ( $list_kabupaten ) {
+            $this->response([
+                'status'    => TRUE,
+                'message'   => 'List kabupaten successfully loaded',
+                'data'      => $list_kabupaten
+            ], REST_Controller::HTTP_OK);
+        }
+    }
 }
--- application/models/api/Customer_model.php
+++ application/models/api/Customer_model.php
@@ -1,5 +1,17 @@
 <?php
 
 class Customer_model extends CI_Model {
-    
+    public function getListProvinsi()
+    {
+        return $this->db->get('list_provinsi')->result_array();
+    }
+
+    public function getListKabupaten($id_provinsi = NULL)
+    {
+        if ( $id_provinsi === NULL ) {
+            return $this->db->get('list_kabupaten')->result_array();
+        } else {
+            return $this->db->get_where('list_kabupaten', ['id_provinsi' => $id_provinsi])->result_array();
+        }
+    }
 }
